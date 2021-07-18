1746d720c0aef888495b7b2d6536c8d614f4a515
OyeniyiOyediran <83999246+OyeniyiOyediran@users.noreply.github.com>
2021-07-18 04:45:43 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace ceasarCipherGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(386, 78);
+            this.button1.MaximumSize = new System.Drawing.Size(174, 36);
+            this.button1.MinimumSize = new System.Drawing.Size(174, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select File";
+            this.toolTip1.SetToolTip(this.button1, "Click to select file you wish to encrypt or decrypt");
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 294);
+            this.button2.MaximumSize = new System.Drawing.Size(212, 36);
+            this.button2.MinimumSize = new System.Drawing.Size(212, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(212, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Encrypt File";
+            this.toolTip1.SetToolTip(this.button2, "Click to create file to write the encrypted text into");
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(347, 294);
+            this.button3.MaximumSize = new System.Drawing.Size(213, 36);
+            this.button3.MinimumSize = new System.Drawing.Size(213, 36);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(213, 36);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Decrypt File";
+            this.toolTip1.SetToolTip(this.button3, "Click to create file to enter the decrypted text into");
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(43, 197);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter Key";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(182, 181);
+            this.textBox1.MaximumSize = new System.Drawing.Size(133, 37);
+            this.textBox1.MinimumSize = new System.Drawing.Size(133, 37);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 37);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.toolTip1.SetToolTip(this.textBox1, "Enter a non negative number to be used as key, must be less than 10");
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(12, 70);
+            this.textBox2.MaximumSize = new System.Drawing.Size(355, 43);
+            this.textBox2.MinimumSize = new System.Drawing.Size(355, 43);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(355, 43);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(584, 346);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(600, 385);
+            this.MinimumSize = new System.Drawing.Size(600, 385);
+            this.Name = "Form1";
+            this.Text = "Ceasar Cipher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ceasarCipherGUI
+{
+    public partial class Form1 : Form
+    {
+        public List<char> allAlphabets = new List<char>()
+            { '[', ']', '!', '|', 'a', 'b', 'Z', 'X', 'C', 'V', 'B', 'N', '0', 'G', 'H', 'J', 'K', 'L', '@', '#', '$', '%', '1', 'M',
+            'Q', 'W', 'E', 'R', 'c', '\'', 'd', 'e', 'f', 'g', 'h','^', '&', '*', '(', ')', '-', 'T', 'Y', 'U', 'I', 'O', 'P',
+            '=',  'i', '6', '7', 'j', 'k', 'l', 'm', 'n', '8', '9', 'o', 'p', '4', '5', 'q', '+', '_', '{', '}', '"', 'A', 'S',
+            'D', 'F', 'r', 's', 't', 'u', 'v', '\\', 'w', '2', '3', 'x', 'y', 'z', ':', '<', '>', '?', '/', '.',',',';'
+    };
+        string fileName = default;
+        int myKey = default;
+        StreamReader myOldFile = default;
+        FileStream myFileStream = default;
+        StreamWriter myNewFile = default;
+        public string cipherFile = default;
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+            button3.Enabled = false;
+            textBox1.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+            using(OpenFileDialog myFile = new OpenFileDialog())
+            {
+                result = myFile.ShowDialog();
+                fileName = myFile.FileName;
+            }
+            if(result is DialogResult.OK && fileName.EndsWith(".txt") is false)
+            {
+                MessageBox.Show("Only \".txt\" files are permitted", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                myFileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                myOldFile = new StreamReader(myFileStream);
+                textBox2.Text = fileName;
+                textBox1.Enabled = true;
+            }
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            myKey = keyVerifier(textBox1.Text);
+            if (Convert.ToInt32(myKey).GetType().ToString() == "System.Int32" && myKey.ToString() != "")
+            {
+                DialogResult choice = default;
+                using (SaveFileDialog encryptedFile = new SaveFileDialog())
+                {
+                    choice = encryptedFile.ShowDialog();
+                    cipherFile = encryptedFile.FileName;
+                }
+
+                if (choice is DialogResult.OK && cipherFile.EndsWith(".txt"))
+                {
+                    myFileStream = new FileStream(cipherFile, FileMode.Create, FileAccess.Write);
+                    myNewFile = new StreamWriter(myFileStream);
+                    StringBuilder encryptedText = new StringBuilder();
+                    int nPosition = default;
+                    string plaintext = myOldFile.ReadToEnd();
+                    string [] bq = Regex.Split(plaintext, @"\s");
+                    foreach (var h in bq)
+                    {
+                        if(h != "")
+                        {
+                            StringBuilder myText= new StringBuilder(h);
+                            for (int i = 0; i < myText.Length; i++)
+                            {
+                                int g = allAlphabets.IndexOf(myText[i]);
+                                nPosition = (g + myKey > allAlphabets.Count()-1) ? (g + myKey) - allAlphabets.Count() : g + myKey;
+                                encryptedText.Append(allAlphabets[nPosition]);
+                            }
+                            try
+                            {
+                                myNewFile.Write(encryptedText + " ");
+                                encryptedText.Clear();
+                            }
+                            catch (Exception)
+                            {
+                                MessageBox.Show("Error occured while writing to file", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                        }
+                    }
+                    try
+                    {
+                        myNewFile.Close();
+                    }
+                    catch (EncoderFallbackException)
+                    {
+                        MessageBox.Show("Unable to close file", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Only \".txt\" files are permitted", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Key must be a number and must not be more than \"10\" ", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            myKey = keyVerifier(textBox1.Text);
+            if (Convert.ToInt32(myKey).GetType().ToString() == "System.Int32" && myKey.ToString() != "")
+            {
+                DialogResult choice = default; 
+                using (SaveFileDialog encryptedFile = new SaveFileDialog())
+                {
+                    choice = encryptedFile.ShowDialog();
+                    cipherFile = encryptedFile.FileName;
+                }
+
+                if (choice is DialogResult.OK && cipherFile.EndsWith(".txt"))
+                {
+                    myFileStream = new FileStream(cipherFile, FileMode.Create, FileAccess.Write);
+                    myNewFile = new StreamWriter(myFileStream);
+                    StringBuilder decryptedText = new StringBuilder();
+                    int nPosition = default;
+                    StringBuilder encodedText = new StringBuilder(myOldFile.ReadToEnd());
+                    string[] zx = Regex.Split(encodedText.ToString(), @"\s");
+                    foreach (var q in zx)
+                    {
+                        if (q != "")
+                        {
+                            StringBuilder plainText = new StringBuilder(q);
+                            for (int i = 0; i < plainText.Length; i++)
+                            {
+                                int g = allAlphabets.IndexOf(plainText[i]);
+                                nPosition = (g - myKey < 0) ? (g - myKey) + allAlphabets.Count() : g - myKey;
+                                decryptedText.Append(allAlphabets[nPosition]);
+                            }
+                            try
+                            {
+                                myNewFile.Write(decryptedText + " ");
+                                decryptedText.Clear();
+                            }
+                            catch (Exception)
+                            {
+                                MessageBox.Show("Error occured while writing to file", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                        }
+                    }                                            
+                    try
+                    {
+                        myNewFile.Close();
+                    }
+                    catch (EncoderFallbackException)
+                    {
+                        MessageBox.Show("Unable to close file", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Only \".txt\" files are permitted", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Key must be a number and must not be more than \"15\" ", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        public int keyVerifier(string k)
+        {
+            int myKey = default;
+            if (!int.TryParse(k, out myKey) || Convert.ToInt32(k) > 10)
+            {
+                MessageBox.Show("Key must be a number and must not be more than \"10\" ", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            return myKey;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            button3.Enabled = true;
+        }
+    }
+}
